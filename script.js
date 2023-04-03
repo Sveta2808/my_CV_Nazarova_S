39543992140ea5db72d43ec7dc35d5f4e228ee17
SvetaNazarova <nazarovas84@gmail.com>
2023-04-03 14:36:30 +0300

    style fixed

---
+++ script.js
@@ -0,0 +1,10 @@
+&:hover {
+    transform: scale(1.5) translate(17%, 7%);
+    box-shadow: 5px 7px 11px 1px #9d91a5;
+    border-radius: 20px;
+}
+
+    
+    
+transition: 2s ease-in-out;
+    z-index: 100;
