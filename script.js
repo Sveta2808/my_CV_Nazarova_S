bdd25ced4bae02293a721566c35aa88e04dfee06
SvetaNazarova <nazarovas84@gmail.com>
2023-04-04 21:50:07 +0300

    letters added

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ const photo = document.querySelector('.my-photo');
 function animation() {
 
     photo.classList.toggle('photo_animation_active');
-    
+
 }
 
 photo.addEventListener('click', animation);
 
+const letter = document.querySelector('.letter');
 
+letter.addEventListener('keydown', (e) => alert('key pressed ' + e.key))
+
+letter.addEventListener('keydown', function (e) {
+    if e.key {letter.scrollIntoView(true);
+});
 
 
 
