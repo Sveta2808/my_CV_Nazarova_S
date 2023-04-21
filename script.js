c45c4800fd87e45c5dd1203c44b43c89351a5feb
SvetaNazarova <nazarovas84@gmail.com>
2023-04-21 14:43:16 +0300

    my repos added

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+const myProject = document.querySelector('.my-project');
+
+myProject.addEventListener('click', fetchAllProject);
+
+async function fetchAllProject() {
+    let url = 'https://api.github.com/users/Sveta2808/repos';
+
+    try {
+        let allProjectRaw = await fetch(url);
+    console.log(allProjectRaw);
+
+    return await allProjectRaw.json();
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+async function fetchDataProject() {
+    let dataProject = await fetchAllProject();
+    let html = '';
+
+    dataProject.forEach(repos => {
+        let htmlSection = `<div class="project-section">
+                            <h2>${repos.full_name} ${repos.html_url}</h2
+                            <div class="description">${repos.description}</div>
+                            </div>`;
+
+        html += htmlSection;
+
+
+    });
+
+    myProject.innerHTML = html;
+
+}
+
+fetchDataProject();
+
+
+
 const photo = document.querySelector('.my-photo');
 
 function animation() {
@@ -22,8 +63,12 @@ document.addEventListener('keydown', function (event) {
         return letter_B.scrollIntoView(true)
     }
     else if (event.code === 'KeyC') {
-        return letter_C.scrollIntoView(true)}
+        return letter_C.scrollIntoView(true)
+    }
     else if (event.code === 'KeyD') {
-        return letter_D.scrollIntoView(true)}
+        return letter_D.scrollIntoView(true)
+    }
 
 });
+
+
